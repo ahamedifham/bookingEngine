053be6f5156edd8af78633ffb704ab43e0c4e4d2
Ahamed Ifham <ifham38@gmail.com>
2022-01-22 17:49:58 +0530

    refactor

--- main.go
+++ main.go
@@ -3,29 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w,"home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w,"about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string){
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-
-	if err != nil {
-		fmt.Println("error passing templates ", err)
-		return
-	}
-}
 
 
 func main() {
@@ -33,7 +13,8 @@ func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	fmt.Printf(fmt.Sprintf("Starting application on port %s", portNumber))
 	http.ListenAndServe(portNumber, nil)
 }
